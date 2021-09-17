5547b6f09a52b7292d6c9a7efa7f3c19c60c9791
Kartavyag5 <Kartavyag5@gmail.com>
2021-09-17 19:27:41 +0530

    don't find table

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
 # import pymysql
 from datetime import datetime
 
@@ -24,28 +25,44 @@ db = SQLAlchemy(app)
 
 # models
 class RID(db.Model):
-    rid = db.Column(db.String(255), unique=True, nullable=False)
-    time_started = db.Column(db.DateTime, default=datetime.datetime.now)
-    time_submitted = db.Column(db.DateTime, default=datetime.datetime.now)
+    rid = db.Column(db.Integer, primary_key=True, unique=True)
+    time_started = db.Column(db.DateTime, default=datetime.now)
+    time_submitted = db.Column(db.DateTime, default=datetime.now)
     
     def __repr__(self):
-        return f"{self.name}"
+        return f"{self.rid}"
 
     def __repr__(self):
         return f"{self.time_started}:{self.time_submitted}"
 
-class Question(db.Model):
-    q1 = db.Column(db.Integer, nullable=False)
-    q2 = db.Column(db.Integer, nullable=False)
-    q3 = db.Column(db.Integer, nullable=False)
-    q4 = db.Column(db.Integer, nullable=False)
+# class Question(db.Model):
+#     q1 = db.Column(db.Integer, nullable=False)
+#     q2 = db.Column(db.Integer, nullable=False)
+#     q3 = db.Column(db.Integer, nullable=False)
+#     q4 = db.Column(db.Integer, nullable=False)
 
-    def __repr__(self):
-        return f"{self.q1}:{self.q2}:{self.q3}:{self.q4}"
+#     def __repr__(self):
+#         return f"{self.q1}:{self.q2}:{self.q3}:{self.q4}"
 
+@app.route('/api')
+def main():
+    return 'Root of Market API'
 
-db.create_all()
+@app.route('/api/start_survey', methods=['GET', 'POST'])
+def start_survey():
+    if request.method=='POST':
+        Rid = request.form['rid']
+        Time_started = request.form['time_started']
+        RID_obj = RID(rid=Rid,)
+        print(RID_obj)
+        db.session.add(RID_obj)  
+        db.session.commit()
+        
+        resp = jsonify({'RID':Rid, 'Time_started':Time_started})
+        return resp
+
 
 # run flask app
 if __name__ == '__main__':
+    #db.create_all()
     app.run(debug=True)
