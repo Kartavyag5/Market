9665f1715c6d5c2ac539b8445615699f3c404c8b
Bhimani Jignesh <83206684+jigneshbhimani@users.noreply.github.com>
2021-09-28 13:52:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ Rid = 'no id'
 
 # models
 class RID(db.Model):
-    id = db.Column(db.Integer, primary_key=True)   
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)   
     rid = db.Column(db.String(200), primary_key=True, unique=True)
     time_started = db.Column(db.DateTime, default=datetime.now)
     time_submitted = db.Column(db.DateTime,nullable=True, default=None))
@@ -38,7 +38,7 @@ class RID(db.Model):
         return f"{self.time_started}:{self.time_submitted}"
 
 class Market(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     rid = db.Column(db.Integer(), db.ForeignKey(RID.id))
     money_bet = db.Column(db.Float())
     prices = db.Column(db.Float())
