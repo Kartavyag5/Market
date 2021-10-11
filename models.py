42b64d0b153d4c1a1dcb1cd6b85f0d192185a599
Bhimani Jignesh <83206684+jigneshbhimani@users.noreply.github.com>
2021-10-11 17:12:51 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,236 @@
+from app import db, app
+from datetime import datetime
+
+""" RID Model """
+class RID(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    rid = db.Column(db.String(200), unique=True)
+    time_started = db.Column(db.DateTime, default=datetime.now)
+    time_submitted = db.Column(db.DateTime, nullable=True, default=None)
+    
+    def __repr__(self):
+        return f"{self.rid}"
+
+    def __repr__(self):
+        return f"{self.time_started}:{self.time_submitted}"
+
+"""
+all market names:
+    1. Kodiak Cakes Concept Test
+    2. Jeopardy!
+    3. BYU Football
+    4. No Time To Die
+    5. 
+    6. 
+    7. 
+    8. 
+    9. 
+    10. 
+"""
+
+""" MARKET-1 Kodiak survery """
+class Market_1(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rid = db.Column(db.Integer(), db.ForeignKey(RID.id),nullable=True)
+    money_bet_1 = db.Column(db.Float())
+    money_bet_2 = db.Column(db.Float())
+    money_bet_3 = db.Column(db.Float())
+    money_bet_4 = db.Column(db.Float())
+    
+    price_1 = db.Column(db.Float())
+    price_2 = db.Column(db.Float())
+    price_3 = db.Column(db.Float())
+    price_4 = db.Column(db.Float())
+    
+    bet_1 = db.Column(db.Integer())
+    bet_2 = db.Column(db.Integer())
+    bet_3 = db.Column(db.Integer())
+    bet_4 = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f"{self.rid.rid}"
+
+
+""" MARKET-2 Jeopardy! """
+class Market_2(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rid = db.Column(db.Integer(), db.ForeignKey(RID.id),nullable=True)
+    money_bet_1 = db.Column(db.Float())
+    money_bet_2 = db.Column(db.Float())
+    money_bet_3 = db.Column(db.Float())
+    money_bet_4 = db.Column(db.Float())
+    
+    price_1 = db.Column(db.Float())
+    price_2 = db.Column(db.Float())
+    price_3 = db.Column(db.Float())
+    price_4 = db.Column(db.Float())
+    
+    bet_1 = db.Column(db.Integer())
+    bet_2 = db.Column(db.Integer())
+    bet_3 = db.Column(db.Integer())
+    bet_4 = db.Column(db.Integer())
+    
+    def __repr__(self):
+        return f"{self.rid.rid}"
+
+
+""" MARKET-3 BYU Football """
+class Market_3(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rid = db.Column(db.Integer(), db.ForeignKey(RID.id),nullable=True)
+    money_bet_1 = db.Column(db.Float())
+    money_bet_2 = db.Column(db.Float())
+    money_bet_3 = db.Column(db.Float())
+    money_bet_4 = db.Column(db.Float())
+    
+    price_1 = db.Column(db.Float())
+    price_2 = db.Column(db.Float())
+    price_3 = db.Column(db.Float())
+    price_4 = db.Column(db.Float())
+    
+    bet_1 = db.Column(db.Integer())
+    bet_2 = db.Column(db.Integer())
+    bet_3 = db.Column(db.Integer())
+    bet_4 = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f"{self.rid.rid}"
+
+    
+""" MARKET-4 No Time To Die """
+class Market_4(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rid = db.Column(db.Integer(), db.ForeignKey(RID.id),nullable=True)
+    money_bet_1 = db.Column(db.Float())
+    money_bet_2 = db.Column(db.Float())
+    
+    price_1 = db.Column(db.Float())
+    price_2 = db.Column(db.Float())
+    
+    bet_1 = db.Column(db.Integer())
+    bet_2 = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f"{self.rid.rid}"
+
+
+""" MARKET-5 """
+class Market_5(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rid = db.Column(db.Integer(), db.ForeignKey(RID.id),nullable=True)
+    money_bet_1 = db.Column(db.Float())
+    money_bet_2 = db.Column(db.Float())
+    money_bet_3 = db.Column(db.Float())
+    
+    price_1 = db.Column(db.Float())
+    price_2 = db.Column(db.Float())
+    price_3 = db.Column(db.Float())
+    
+    bet_1 = db.Column(db.Integer())
+    bet_2 = db.Column(db.Integer())
+    bet_3 = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f"{self.rid.rid}"
+
+
+""" MARKET-6 """
+class Market_6(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rid = db.Column(db.Integer(), db.ForeignKey(RID.id),nullable=True)
+    money_bet_1 = db.Column(db.Float())
+    money_bet_2 = db.Column(db.Float())
+    money_bet_3 = db.Column(db.Float())
+    
+    price_1 = db.Column(db.Float())
+    price_2 = db.Column(db.Float())
+    price_3 = db.Column(db.Float())
+    
+    bet_1 = db.Column(db.Integer())
+    bet_2 = db.Column(db.Integer())
+    bet_3 = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f"{self.rid.rid}"
+
+
+""" MARKET-7 """
+class Market_7(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rid = db.Column(db.Integer(), db.ForeignKey(RID.id),nullable=True)
+    money_bet_1 = db.Column(db.Float())
+    money_bet_2 = db.Column(db.Float())
+    money_bet_3 = db.Column(db.Float())
+    
+    price_1 = db.Column(db.Float())
+    price_2 = db.Column(db.Float())
+    price_3 = db.Column(db.Float())
+    
+    bet_1 = db.Column(db.Integer())
+    bet_2 = db.Column(db.Integer())
+    bet_3 = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f"{self.rid.rid}"
+
+
+""" MARKET-8 """
+class Market_8(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rid = db.Column(db.Integer(), db.ForeignKey(RID.id),nullable=True)
+    money_bet_1 = db.Column(db.Float())
+    money_bet_2 = db.Column(db.Float())
+    money_bet_3 = db.Column(db.Float())
+    
+    price_1 = db.Column(db.Float())
+    price_2 = db.Column(db.Float())
+    price_3 = db.Column(db.Float())
+    
+    bet_1 = db.Column(db.Integer())
+    bet_2 = db.Column(db.Integer())
+    bet_3 = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f"{self.rid.rid}"
+
+
+""" MARKET-9 """
+class Market_9(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rid = db.Column(db.Integer(), db.ForeignKey(RID.id),nullable=True)
+    money_bet_1 = db.Column(db.Float())
+    money_bet_2 = db.Column(db.Float())
+    money_bet_3 = db.Column(db.Float())
+    
+    price_1 = db.Column(db.Float())
+    price_2 = db.Column(db.Float())
+    price_3 = db.Column(db.Float())
+    
+    bet_1 = db.Column(db.Integer())
+    bet_2 = db.Column(db.Integer())
+    bet_3 = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f"{self.rid.rid}"
+
+
+""" MARKET-10 """
+class Market_10(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rid = db.Column(db.Integer(), db.ForeignKey(RID.id),nullable=True)
+    money_bet_1 = db.Column(db.Float())
+    money_bet_2 = db.Column(db.Float())
+    money_bet_3 = db.Column(db.Float())
+    
+    price_1 = db.Column(db.Float())
+    price_2 = db.Column(db.Float())
+    price_3 = db.Column(db.Float())
+    
+    bet_1 = db.Column(db.Integer())
+    bet_2 = db.Column(db.Integer())
+    bet_3 = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f"{self.rid.rid}"
+
+db.create_all()
