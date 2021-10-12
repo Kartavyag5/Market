0f7acb3ccf7dcc2827df0c2147ff08d069cd3c35
Bhimani Jignesh <83206684+jigneshbhimani@users.noreply.github.com>
2021-10-12 11:14:27 +0530

    Create constant.py and store the messages

---
+++ constant.py
@@ -0,0 +1,13 @@
+"""
+
+ERROR_MESSAGE : {
+    "rid_used" : "RID already exists use different one."
+    "id" : "ID not found."
+    "start_survey" : "No user start survey."
+}
+
+SUCCESS_MESSAGE : {
+    "rid_created" : "New RID Created."
+}
+
+"""
